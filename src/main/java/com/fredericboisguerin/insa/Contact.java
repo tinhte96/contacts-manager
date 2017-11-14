ae22623124c983804582ca36cdf3ae74e7c89d6f
tinhte96 <vanthiennhat@gmail.com>
2017-11-14 14:51:00 +0100

    change class name to Contact

---
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -0,0 +1,38 @@
+package com.fredericboisguerin.insa;
+
+public class Contact {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Contact(String pName, String pEmail, String pPhoneNumber){
+        this.name = pName;
+        this.email = pEmail;
+        this.phoneNumber = pPhoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
