02ae95e5c0024032d168369a5c505e83cdb0c508
tinhte96 <vanthiennhat@gmail.com>
2017-11-17 16:46:11 +0100

    test edit contact and delete contact

--- src/test/java/com/fredericboisguerin/insa/ContactsManagerShould.java
+++ src/test/java/com/fredericboisguerin/insa/ContactsManagerShould.java
@@ -2,6 +2,8 @@ package com.fredericboisguerin.insa;
 
 import org.junit.Test;
 
+import javax.naming.InvalidNameException;
+
 public class ContactsManagerShould {
 
     private static final String SOME_VALID_EMAIL = "toto@titi.fr";
@@ -9,7 +11,7 @@ public class ContactsManagerShould {
     private static final String SOME_VALID_NAME = "Toto titi";
 
     @Test(expected = InvalidContactNameException.class)
-    public void fail_if_no_name() {
+    public void fail_if_no_name() throws Exception {
         ContactsManager contactsManager = new ContactsManager();
         String noName = null;
 
@@ -17,7 +19,7 @@ public class ContactsManagerShould {
     }
 
     @Test(expected = InvalidContactNameException.class)
-    public void fail_if_name_is_empty() {
+    public void fail_if_name_is_empty() throws InvalidEmailException, InvalidContactNameException {
         ContactsManager contactsManager = new ContactsManager();
         String emptyName = "";
 
@@ -25,10 +27,30 @@ public class ContactsManagerShould {
     }
 
     @Test(expected = InvalidEmailException.class)
-    public void fail_if_email_is_not_valid() {
+    public void fail_if_email_is_not_valid() throws InvalidEmailException, InvalidContactNameException {
         ContactsManager contactsManager = new ContactsManager();
         String invalidEmail = "tototitifr";
 
         contactsManager.addContact(SOME_VALID_NAME, invalidEmail, SOME_VALID_PHONE_NUMBER);
     }
+
+    @Test(expected = InvalidContactNameException.class)
+    public void fail_if_name_is_not_vali_when_edit() throws InvalidContactNameException, InvalidEmailException{
+
+        ContactsManager contactsManager = new ContactsManager();
+        contactsManager.addContact(SOME_VALID_NAME,SOME_VALID_EMAIL,SOME_VALID_PHONE_NUMBER);
+
+        String invalidName = "toto";
+        contactsManager.editContact(invalidName,SOME_VALID_EMAIL,SOME_VALID_PHONE_NUMBER);
+    }
+
+    @Test(expected = InvalidEmailException.class)
+    public void fail_if_email_is_not_valid_when_edit() throws InvalidEmailException, InvalidContactNameException{
+        ContactsManager contactsManager = new ContactsManager();
+        contactsManager.addContact(SOME_VALID_NAME,SOME_VALID_EMAIL,SOME_VALID_PHONE_NUMBER);
+
+        String invalidEmail = "toto.titi";
+        contactsManager.editContact(SOME_VALID_NAME,invalidEmail,SOME_VALID_PHONE_NUMBER);
+    }
+
 }
