398b3a4845915e0a92733c5e19fcf8d1ee044809
tinhte96 <vanthiennhat@gmail.com>
2017-11-17 16:46:35 +0100

    test askInfoForContact

--- src/test/java/com/fredericboisguerin/insa/UserInterfaceTest.java
+++ src/test/java/com/fredericboisguerin/insa/UserInterfaceTest.java
@@ -10,15 +10,15 @@ public class UserInterfaceTest extends ContactsManagerMust{
     @Test
     public void type_contact_return_new_contact()throws InvalidContactNameException, InvalidEmailException {
 
+        System.setIn(new ByteArrayInputStream("name\nemail@domain.fr\nphone\n".getBytes()));
+
         ContactsManager contactsManager = Mockito.mock(ContactsManager.class);
         UserInterface ui = new UserInterface();
+        ui.askInfoForContact(contactsManager);
 
-        System.setIn(new ByteArrayInputStream("name\nemail\nphone\n".getBytes()));
-        ui.type_contact();
-
-        String name="";
-        String email="";
-        String phoneNumber="";
+        String name="name";
+        String email="email@domain.fr";
+        String phoneNumber="phone";
         Mockito.verify(contactsManager).addContact(name,email,phoneNumber);
     }
 
