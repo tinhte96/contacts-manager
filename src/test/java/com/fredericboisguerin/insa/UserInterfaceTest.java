6f8dafa00f91adf342549c82e16ba47ac7a5765c
tinhte96 <vanthiennhat@gmail.com>
2017-11-23 17:19:09 +0100

    test ui

--- src/test/java/com/fredericboisguerin/insa/UserInterfaceTest.java
+++ src/test/java/com/fredericboisguerin/insa/UserInterfaceTest.java
@@ -8,13 +8,14 @@ import java.io.ByteArrayInputStream;
 public class UserInterfaceTest extends ContactsManagerMust{
 
     @Test
-    public void type_contact_return_new_contact()throws InvalidContactNameException, InvalidEmailException {
+    public void type_contact_return_new_contact()
+            throws InvalidContactNameException, InvalidEmailException {
 
         System.setIn(new ByteArrayInputStream("name\nemail@domain.fr\nphone\n".getBytes()));
 
         ContactsManager contactsManager = Mockito.mock(ContactsManager.class);
         UserInterface ui = new UserInterface();
-        ui.askInfoForContact(contactsManager);
+        ui.askInfoForAddingContact(contactsManager);
 
         String name="name";
         String email="email@domain.fr";
@@ -22,4 +23,33 @@ public class UserInterfaceTest extends ContactsManagerMust{
         Mockito.verify(contactsManager).addContact(name,email,phoneNumber);
     }
 
+    @Test
+    public void type_contact_return_new_edited_contact()
+            throws InvalidEmailException, InvalidContactNameException{
+
+        System.setIn(new ByteArrayInputStream("name\nemail@domain.fr\nphone\n".getBytes()));
+
+        ContactsManager contactsManager = Mockito.mock(ContactsManager.class);
+        UserInterface ui = new UserInterface();
+        ui.askInfoForEditingContact(contactsManager);
+
+        String name="name";
+        String email="email@domain.fr";
+        String phoneNumber="phone";
+        Mockito.verify(contactsManager).editContact(name,email,phoneNumber);
+    }
+
+    @Test
+    public void type_contact_and_delete_contact()
+            throws InvalidEmailException, InvalidContactNameException{
+
+        System.setIn(new ByteArrayInputStream("name\nY\n".getBytes()));
+
+        ContactsManager contactsManager = Mockito.mock(ContactsManager.class);
+        UserInterface ui = new UserInterface();
+        ui.askInfoForDeletingContact(contactsManager);
+
+        String name="name";
+        Mockito.verify(contactsManager).deleteContact(name);
+    }
 }
