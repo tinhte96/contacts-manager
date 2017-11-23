563d26707d8cda50e4aee6413c40605896ff413a
tinhte96 <vanthiennhat@gmail.com>
2017-11-23 17:18:11 +0100

    ui for asking, editing, deleting and searching contact

--- src/main/java/com/fredericboisguerin/insa/UserInterface.java
+++ src/main/java/com/fredericboisguerin/insa/UserInterface.java
@@ -4,18 +4,68 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-        Scanner sc;
+    Scanner sc;
 
-        public UserInterface (){
+    public UserInterface (){
             sc = new Scanner(System.in);
         }
 
-        public void askInfoForContact(ContactsManager contactsManager) throws InvalidContactNameException, InvalidEmailException{
-            System.out.println("Votre information : ");
-            String name = sc.nextLine();
-            String email = sc.nextLine();
-            String phoneNumber = sc.nextLine();
-            System.out.println(name+email+phoneNumber);
-            contactsManager.addContact(name,email,phoneNumber);
+    public void askInfoForAddingContact(ContactsManager contactsManager)
+            throws InvalidContactNameException, InvalidEmailException{
+        System.out.println("Your information : ");
+        System.out.print("name : ");
+        String name = sc.nextLine();
+        System.out.print("email : ");
+        String email = sc.nextLine();
+        System.out.print("phone number : ");
+        String phoneNumber = sc.nextLine();
+        System.out.println("\n");
+        contactsManager.addContact(name,email,phoneNumber);
+    }
+
+    public void askInfoForEditingContact(ContactsManager contactsManager)
+            throws InvalidContactNameException, InvalidEmailException{
+        System.out.println("Your information : ");
+        System.out.print("name : ");
+        String name = sc.nextLine();
+        System.out.print("email : ");
+        String email = sc.nextLine();
+        System.out.print("phone number : ");
+        String phoneNumber = sc.nextLine();
+        System.out.println("\n");
+        contactsManager.editContact(name,email,phoneNumber);
+    }
+
+    public void askInfoForDeletingContact(ContactsManager contactsManager)
+            throws InvalidContactNameException, InvalidEmailException{
+        System.out.println("Your information : ");
+        System.out.print("name : ");
+        String name = sc.nextLine();
+
+        while (true) {
+            System.out.println("Are you sure you want to delete this user : " + name + " [Y\\N]");
+            String confirm = sc.nextLine();
+            if (confirm.toLowerCase().equals("y")) {
+                contactsManager.deleteContact(name);
+                break;
+            } else if (confirm.toLowerCase().equals("n")) {
+                System.out.println("Nothing done");
+                break;
+            } else {
+                System.out.println("Please tape Y or N ");
+            }
         }
+    }
+
+    public void searchContactByName(ContactsManager contactsManager){
+        System.out.println("Your information : ");
+        System.out.print("name : ");
+        String name = sc.nextLine();
+        System.out.println("\n");
+
+        contactsManager.searchContactByName(name);
+
+    }
+
+
 }
